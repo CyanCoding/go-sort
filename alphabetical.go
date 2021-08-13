4a9254786896c66977c2e4b487c81bb85268262c
Camden Mac Leod <camden.macleod@pm.me>
2021-08-13 09:29:28 -0400

    Add alphabet

--- alphabetical.go
+++ alphabetical.go
@@ -1,5 +1,11 @@
 package gosort
 
+var alphabet = [26]string{
+	"a", "b", "c", "d", "e", "f", "g", "h", "i",
+	"j", "k", "l", "n", "n", "o", "p", "q", "r",
+	"s", "t", "u", "v", "w", "x", "y", "z",
+}
+
 func SortString(word string) string {
 
 }
